0bcca1e70f7441dfc18935753995aa0e3cf7ea5f
Lucas Pittam <98059989+lucaspittam@users.noreply.github.com>
2022-03-29 11:48:58 -0400

    update
    body card

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -10,4 +10,21 @@ var getUserRepos = (user) => {
     });
 };
 
-getUserRepos();
+var userFormEl = document.querySelector("#user-form")
+var nameInputEl = document.querySelector("#username")
+
+var formSubmitHandler = (event) => {
+    event.preventDefault();
+    // get value from input element
+var username = nameInputEl.value.trim();
+
+if (username) {
+  getUserRepos(username);
+  nameInputEl.value = "";
+} else {
+  alert("Please enter a GitHub username");
+}
+    console.log(event);
+  };
+
+  userFormEl.addEventListener("submit", formSubmitHandler);
